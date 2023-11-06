5fe48a83890afd96312084e26d3d2f9afebe8095
SaneMyburg <sane.myburg@gmail.com>
2023-11-06 14:35:44 +0200

    Add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navbar';
+import Hello from './Hello';
+
+const Home = () => (
+  <>
+    <Navigation />
+    <Hello />
+  </>
+);
+
+export default Home;
